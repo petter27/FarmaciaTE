1f2befcccbbbf48e68d1beb09cba18324c696e31
jrsvaldez <jrsifontes16@gmail.com>
2019-03-28 12:02:03 -0600

    cambios grafico sal login

--- login.php
+++ login.php
@@ -1,49 +1,47 @@
 <?php
-        $resultado="";
-        if (isset($_POST["login"])){
-            session_start();
-            $usuario=$_POST["usuario"];
-            $password=$_POST["password"];
-            
+$resultado = "";
+if (isset($_POST["login"])) {
+  session_start();
+  $usuario = $_POST["usuario"];
+  $password = $_POST["password"];
 
-            try{
-                require_once("includes/functions/bd_conexion.php");
-                $stmt=$conn->prepare("SELECT usr_id, usr_nombre, usr_password, usr_tipo FROM usuario WHERE usr_nombre=? 
+
+  try {
+    require_once("includes/functions/bd_conexion.php");
+    $stmt = $conn->prepare("SELECT usr_id, usr_nombre, usr_password, usr_tipo FROM usuario WHERE usr_nombre=? 
                 AND usr_estado=1;");
-                $stmt->bind_param("s", $usuario);
-                $stmt->execute();
-                $stmt->bind_result($id,$nombre_usr, $password_usr, $tipo_usr);
-                
-                while($stmt->fetch()){
+    $stmt->bind_param("s", $usuario);
+    $stmt->execute();
+    $stmt->bind_result($id, $nombre_usr, $password_usr, $tipo_usr);
 
-                    if(password_verify($password,$password_usr)){
+    while ($stmt->fetch()) {
 
-                      $user= array (
-                        'id' => $id,
-                        'nombre' => $nombre_usr,
-                        'tipo' => $tipo_usr
-                      );
-                        
-                        if($tipo_usr==1){
-                        $_SESSION['usr_admin']=$user;
-                        header('Location: index.php');
-                      }elseif($tipo_usr==2){
-                        $_SESSION['usr_emp']=$user;
-                        header('Location: index_emp.php');
-                      }
-                    }else{
-                        $resultado="Hubo un error";
-                    }
-                
-                }
-                    
-                $stmt->close();
-                $conn->close();
-            }catch(Exception $e){
-                echo "Error: " . $e->getMessage();
-            }
-            
+      if (password_verify($password, $password_usr)) {
+
+        $user = array(
+          'id' => $id,
+          'nombre' => $nombre_usr,
+          'tipo' => $tipo_usr
+        );
+
+        if ($tipo_usr == 1) {
+          $_SESSION['usr_admin'] = $user;
+          header('Location: index.php');
+        } elseif ($tipo_usr == 2) {
+          $_SESSION['usr_emp'] = $user;
+          header('Location: index_emp.php');
         }
+      } else {
+        $resultado = "Hubo un error";
+      }
+    }
+
+    $stmt->close();
+    $conn->close();
+  } catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+  }
+}
 ?>
 
 
@@ -52,96 +50,78 @@
 
 <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <title>SB Admin 2 - Login</title>
+    <title>SB Admin 2 - Login</title>
 
-  <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
 <body class="bg-gradient-primary">
 
-  <div class="container">
+    <div class="container">
 
-    <!-- Outer Row -->
-    <div class="row justify-content-center">
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
 
-      <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-xl-10 col-lg-12 col-md-9">
 
-        <div class="card o-hidden border-0 shadow-lg my-5">
-          <div class="card-body p-0">
-            <!-- Nested Row within Card Body -->
-            <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-              <div class="col-lg-6">
-                <div class="p-5">
-                  <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                  </div>
-                  <form class="user" action="login.php" method="POST">
-                    <div class="form-group">
-                      <input type="text" class="form-control form-control-user" name="usuario" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Ingrese su usuario...">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block">
+                                <img src="img/login_img.png"></img>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Farmacia TE</h1>
+                                    </div>
+                                    <form class="user" action="login.php" method="POST">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user" name="usuario" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Ingrese su usuario...">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user" name="password" id="exampleInputPassword" placeholder="contraseña">
+                                        </div>
+
+                                        <input type="submit" name="login" class="btn btn-primary btn-user btn-block" value="iniciar sesion">
+                                        <hr>
+                                    </form>
+                                    <hr>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="form-group">
-                      <input type="password" class="form-control form-control-user" name="password" id="exampleInputPassword" placeholder="contraseña">
-                    </div>
-                    <div class="form-group">
-                      <div class="custom-control custom-checkbox small">
-                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                        <label class="custom-control-label" for="customCheck">Remember Me</label>
-                      </div>
-                    </div>
-                    
-                    <input type="submit" name="login" class="btn btn-primary btn-user btn-block" value="iniciar sesion">
-                      Login
-                    </a>
-                    <hr>
-                    <a href="index.html" class="btn btn-google btn-user btn-block">
-                      <i class="fab fa-google fa-fw"></i> Login with Google
-                    </a>
-                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                    </a>
-                  </form>
-                  <hr>
-                  <div class="text-center">
-                    <a class="small" href="forgot-password.html">Forgot Password?</a>
-                  </div>
-                  <div class="text-center">
-                    <a class="small" href="register.html">Create an Account!</a>
-                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
 
-      </div>
+            </div>
+
+        </div>
 
     </div>
 
-  </div>
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-  <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
-  <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
 
 </body>
 
-</html>
+</html> 
